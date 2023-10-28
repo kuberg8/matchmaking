7e37f7e61454ec673ff42ec0dc4dc95796afc9b9
Алексей Демиденко <kuberg8@gmail.com>
2023-10-28 18:13:40 +0500

    fix

--- store/user.js
+++ store/user.js
@@ -2,10 +2,14 @@ export const state = () => ({})
 
 export const getters = {}
 
-export const mutations = {}
+export const mutations = {
+  SET_USER_DATA(state, userData) {
+    state.userData = userData
+  }
+}
 
 export const actions = {
-  async getUserInfo(token) {
+  async getUserInfo({ commit }, token) {
     const { data } = await this.$axios.$get(
       `https://login.yandex.ru/info?format=json&jwt_secret=${process.env.JWT_SECRET}`,
       {
@@ -15,5 +19,6 @@ export const actions = {
       }
     )
     console.log(data)
+    commit('SET_USER_DATA', data)
   }
 }
