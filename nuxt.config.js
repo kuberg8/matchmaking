bf619e8a25de0b97fffee5e7bbaef1cc95c28ed7
Алексей Демиденко <kuberg8@gmail.com>
2024-09-21 01:58:56 +0300

    disable user-scalable

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,10 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: '' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
